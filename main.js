c88b7b6db073f7c27fab604c4c53772eff315e0d
Kaj Oskar Rusilowski <xedom08@gmail.com>
2019-04-03 22:40:09 +0200

    navbar animation with js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log('ok');
+
+const navButton = document.querySelector('header nav #logo #menuicon');
+const navContainer = document.querySelector('header nav');
+
+navButton.addEventListener('click', (x) => {
+    if (navContainer.classList.contains('exp'))
+        return navContainer.classList.remove('exp');
+    return navContainer.classList.add('exp');
+});
