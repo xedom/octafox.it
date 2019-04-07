78f1b143dc10e19e13a8d37f6bd9c369fe1841b5
xedom <xedom08@gmail.com>
2019-04-07 03:48:54 +0200

    css / scss  loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,18 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
