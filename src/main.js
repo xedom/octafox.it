31fd9c19e13f1a4216a17ca0a1ca95811aec55f5
xedom <xedom08@gmail.com>
2019-04-07 05:28:23 +0200

    bho

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
 import './css/style.scss';
 
 window.addEventListener('load', function () {
-
     const navButton = document.querySelector('header nav #logo #menuicon');
     const navContainer = document.querySelector('header nav');
 
     navButton.addEventListener('click', (x) => {
         if (navContainer.classList.contains('exp')) {
-            console.log(navContainer.classList.contains('exp'))
-            navContainer.classList.remove('exp')
+            console.log(navContainer.classList.contains('exp'));
+            navContainer.classList.remove('exp'),
         } else {
-            console.log(navContainer.classList.contains('exp'))
-            navContainer.classList.add('exp')
-        }
+            console.log(navContainer.classList.contains('exp'));
+            navContainer.classList.add('exp');
+        };
     });
-})
-
+});
