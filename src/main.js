772a9853e674f1e98c74ad6fa7296d4c33d002a8
xedom <xedom08@gmail.com>
2019-04-22 22:33:26 +0200

    refaktor

--- src/main.js
+++ src/main.js
@@ -4,13 +4,8 @@ window.addEventListener('load', function () {
     const navButton = document.querySelector('header nav #logo #menuicon');
     const navContainer = document.querySelector('header nav');
 
-    navButton.addEventListener('click', (x) => {
-        if (navContainer.classList.contains('exp')) {
-            console.log(navContainer.classList.contains('exp'));
-            navContainer.classList.remove('exp');
-        } else {
-            console.log(navContainer.classList.contains('exp'));
-            navContainer.classList.add('exp');
-        };
+    navButton.addEventListener('click', x => {
+        if (navContainer.classList.contains('exp')) navContainer.classList.remove('exp');
+        else navContainer.classList.add('exp');
     });
 });
