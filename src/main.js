99d268ca668c010a6af1a8d112607fec1a890bd8
xedom <xedom08@gmail.com>
2019-04-25 00:59:24 +0200

    function to add card

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import './css/style.scss';
 
+import cards from './data/cards.json';
+
 const navContainer = document.querySelector('header nav');
 const navButton = navContainer.querySelector('#logo #menuicon');
 const navItemsContainer = navContainer.querySelector('ul');
 const sections = document.querySelectorAll('main section');
+const sProgetti = document.querySelector('section#progetti .content .cards');
 
 window.addEventListener('load', init);
 
@@ -14,6 +17,8 @@ function init() {
     });
 
     addMenuItem();
+
+    createCard(cards.progetti[0]);
 };
 
 function addMenuItem() {
@@ -30,5 +35,25 @@ function addMenuItem() {
 };
 
 function createCard({ image, title, description }) {
+    sProgetti.innerHTML = "";
 
+    const ccCard = document.createElement('DIV');
+    ccCard.className = 'card';
+    const cccImg = document.createElement('IMG');
+    console.log(image);
+    console.log(title);
+    console.log(description);
+    cccImg.src = image;
+
+    const cccTit = document.createElement('H3');
+    cccTit.textContent = title;
+
+    const cccDes = document.createElement('P');
+    cccDes.textContent = description;
+
+    ccCard.appendChild(cccImg);
+    ccCard.appendChild(cccTit);
+    ccCard.appendChild(cccDes);
+
+    sProgetti.appendChild(ccCard);
 };
