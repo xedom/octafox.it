6745377cf9bb80efa7d3578d91586a7d56701884
xedom <xedom08@gmail.com>
2019-04-25 00:34:23 +0200

    function to add automatically link to navbar

--- src/main.js
+++ src/main.js
@@ -1,19 +1,32 @@
 import './css/style.scss';
 
-window.addEventListener('load', menuSlider);
+const navContainer = document.querySelector('header nav');
+const navButton = navContainer.querySelector('#logo #menuicon');
+const navItemsContainer = navContainer.querySelector('ul');
+const sections = document.querySelectorAll('main section');
 
-function menuSlider() {
-    const navButton = document.querySelector('header nav #logo #menuicon');
-    const navContainer = document.querySelector('header nav');
+window.addEventListener('load', init);
 
+function init() {
     navButton.addEventListener('click', x => {
         if (navContainer.classList.contains('exp')) navContainer.classList.remove('exp');
         else navContainer.classList.add('exp');
     });
+
+    addMenuItem();
 };
 
 function addMenuItem() {
-    
+    navItemsContainer.innerHTML = "";
+
+    sections.forEach(menuLink => {
+        const li = document.createElement('LI');
+        const a = document.createElement('A');
+        a.href = `#${menuLink.id}`;
+        a.textContent = (menuLink.id).toUpperCase();
+        li.appendChild(a);
+        navItemsContainer.appendChild(li);
+    });
 };
 
 function createCard({ image, title, description }) {
